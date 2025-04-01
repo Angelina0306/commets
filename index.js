12c186d7864d3e18c04e4f14ebf4a1c34672238d
Angelina0306 <167233644+Angelina0306@users.noreply.github.com>
2025-04-01 20:14:03 +0300

    Initial commit (3.6)

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ import { addComment } from "./modules/comments.js";
 
 export const init = async () => {
   const addButton = document.querySelector(".add-form-button");
+  const loadingContainer = document.querySelector(".loading-container");
+  const commentsContainer = document.querySelector(".comments");
   
   addButton.addEventListener("click", handleAddComment);
   
   const comments = await getCommentsFromServer();
+
+  loadingContainer.style.display = "none";
+  commentsContainer.style.display = "flex";
 
   comments.forEach(comment => {
     addComment(comment);
