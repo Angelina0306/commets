22a7c45e01790b6e8072339ba5b1e227f6b0f6e1
Angelina0306 <167233644+Angelina0306@users.noreply.github.com>
2025-04-18 17:51:08 +0300

    hw_3.7ф

--- modules/api.js
+++ modules/api.js
@@ -1,10 +1,16 @@
 const API_URL = "https://wedev-api.sky.pro/api/v1/angelina/comments";
 
 export const getCommentsFromServer = async () => {
-  const response = await fetch(API_URL);
+  let response;
+  try {
+    response = await fetch(API_URL);
+  } catch (error) {
+    throw new Error("У вас пропал интернет. Ошибка: " + error.message);
+  }
   if (response.status === 500) {
     throw new Error("Сервер сломался");
   }
+  // 400-е невозможны, так как мы не передаём никаких параметров
   const data = await response.json();
   return data.comments;
 };
@@ -17,7 +23,7 @@ export const addCommentToServer = async (name, text) => {
       body: JSON.stringify({ name, text, forceError: true }),
     });
   } catch (error) {
-    throw new Error("У вас пропал интернет");
+    throw new Error("У вас пропал интернет. Ошибка: " + error.message);
   }
     if (response.status >= 500) {
       throw new Error("Сервер сломался");
--- modules/handlers.js
+++ modules/handlers.js
@@ -7,6 +7,19 @@ import {
 import { renderComments } from "./render.js";
 import { addCommentToServer } from "./api.js";
 
+const escapeHTML = (str) => {
+  return str.replace(/[&<>'"]/g, (tag) => {
+    const tagsToReplace = {
+      "&": "&amp;",
+      "<": "&lt;",
+      ">": "&gt;",
+      "'": "&#39;",
+      '"': "&quot;",
+    };
+    return tagsToReplace[tag] || tag;
+  });
+};
+
 export const handleLikeClick = (event) => {
   const commentElement = event.target.closest(".comment");
   const index = commentElement.dataset.index;
@@ -42,8 +55,8 @@ export const handleAddComment = async (event) => {
   const loadingContainer = document.querySelector(".loading-container");
   const form = document.querySelector(".add-form");
 
-  const name = nameInput.value.trim();
-  const text = textInput.value.trim();
+  const name = escapeHTML(nameInput.value.trim());
+  const text = escapeHTML(textInput.value.trim());
   if (name.length < 3 || text.length < 3) {
     alert("Текст и имя должны быть длиннее 3 символов.");
     return;
